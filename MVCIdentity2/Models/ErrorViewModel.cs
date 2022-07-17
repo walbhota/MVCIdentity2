fe4b3915e8bf033e3b3b8db366d94d21582c97a0
j8 <walbhota@gmail.com>
2022-07-17 05:49:05 +0100

    Add project files.

---
+++ MVCIdentity2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVCIdentity2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
